fd135bf769a58581e271403b4a4a9442ffc735d1
Sarobidy RANAIVOSAMINANA <sarobidymanana18@gmail.com>
2021-12-02 11:17:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join(__dirname, "public")))
+app.get("/", async (req, res) => {
+    console.log(path.join(__dirname, "public"));
+    res.sendFile(path.join(__dirname, "publi","index.html"));
+})
+
+app.listen(port, () => {
+    console.log(`app listen on port : ${port}`);
+})
