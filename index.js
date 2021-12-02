b18f982f7e46b8962b290a7a645fccdb1d03973f
Sarobidy RANAIVOSAMINANA <sarobidymanana18@gmail.com>
2021-12-02 12:47:19 +0100

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const Port = process.env || 5000;
+const Port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")))
 app.set("views",path.join(__dirname, "public"))
